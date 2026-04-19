40d19251be991b82872d4054c6a0f04964c1a583
cherrymaellimbaga-sudo <cherrymaellimbaga@gmail.com>
2026-04-19 23:56:28 +0800

    File Handling Mobile (Deletion of Frontend - Deployment and FrontendMobile - Deployment)

--- Frontend - Deployment/src/components/resetPassForm.jsx
+++
@@ -1,7 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-const ResetPasswordPage = () => {
-  return <Navigate to={`/landing${window.location.search}`} replace />;
-};
-
-export default ResetPasswordPage;
